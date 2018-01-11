ceca35cc116bf16a1993e7eea32eb104500b66e9
armandocano91 <armandocano.ac@gmail.com>
2018-01-11 15:41:57 -0600

    Finished queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,34 @@
 var Queue = function() {
-  var someInstance = {};
+ /*Queue === "FIFO"*/
+ var someInstance = {};
 
-  // Use an object with numeric keys to store values
-  var storage = {};
+ // Use an object with numeric keys to store values
+ var storage = {};
+ var lastIndex = 0; //next person in line
+ var index = 0; // first person in line
+ var length = 0; //how many people are in line
 
-  // Implement the methods below
+ // Implement the methods below
 
-  someInstance.enqueue = function(value) {
-  };
+ someInstance.enqueue = function(value) {
+   // .enqueue adds "value" to the back
+   storage[index++] = value;
+   length++;
+ };
 
-  someInstance.dequeue = function() {
-  };
+ someInstance.dequeue = function() {
+   // .dequeue removes "values" from the front
+   if (length > 0) {
+     var result = storage[lastIndex];
+     delete storage[lastIndex++]
+     length--;
+     return result;
+   }
+ };
+ someInstance.size = function() {
+   // returns inputs and outputs
+   return length;
+ };
 
-  someInstance.size = function() {
-  };
-
-  return someInstance;
+ return someInstance;
 };
